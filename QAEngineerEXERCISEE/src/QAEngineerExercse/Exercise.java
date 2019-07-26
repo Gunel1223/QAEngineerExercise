f25468ae6712c02b1f0d8ec3fd8cfd5128d043a7
Gunel1223 <53346746+Gunel1223@users.noreply.github.com>
2019-07-26 12:36:43 -0400

    Add files via upload

---
+++ QAEngineerEXERCISEE/src/QAEngineerExercse/Exercise.java
@@ -0,0 +1,33 @@
+package QAEngineerExercse;
+
+public class Exercise {
+
+    public static void main(String[] args) {
+        System.out.println(printLongestWord("The cow jumped over the moon......."));
+
+    }
+/*Estimated Time: 30-60 mins depending on experience.
+
+1.	In the programming language of your choice create a class with a method to return the length and longest words in a sentence.
+For example, “The cow jumped over the moon.” should return 6 and “jumped”.
+2.	Create unit tests to test that method, reworking your code if needed.
+3.	Explain any assumptions in comments.
+4.	Add a README explaining how to execute your tests.
+5.	Share via GitHub etc.*/
+
+public static String printLongestWord(String sentence) {
+
+    String[] words = sentence.replaceAll("[^a-zA-Z0-9]", " ").split(" ");
+    String longestWord = words[0];
+    for (int i = 0; i < words.length; i++) {
+        if (longestWord.length() <= words[i].length()) {
+            longestWord = words[i];
+
+        }
+    }
+    return longestWord.length()+" "+ longestWord;
+
+}
+
+
+}
